aa754136864da4de4cd7f2900a6e0330ae6bc06f
SarahisCode <111197143+SarahisCode@users.noreply.github.com>
2024-11-17 20:55:55 -0500

    Update quest1p2.py

--- quest1p2.py
+++ quest1p2.py
@@ -1,6 +1,6 @@
 str = input()
 pots = 0
-for point in range(0, len(str)-1, 2)
+for point in range(0, len(str)-1, 2):
     to_consider = str[point:point+2]
     pots += to_consider.count("B")
     pots += 3*to_consider.count("C")
